11078a3e68d1de9431c1dca659131d5d3aa03144
Basil Peace <grv87@yandex.ru>
2019-04-15 11:48:57 +0300

    WIP

--- src/main/groovy/org/fidata/gradle/about/AboutPlugin.groovy
+++ src/main/groovy/org/fidata/gradle/about/AboutPlugin.groovy
@@ -1,3 +1,15 @@
 package org.fidata.gradle.about
-class AboutPlugin {
+
+import groovy.transform.CompileStatic
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+@CompileStatic
+class AboutPlugin implements Plugin<Project> {
+  public static final ABOUT_EXTENSION_NAME = 'about'
+
+  @Override
+  void apply(Project project) {
+    project.extensions.create ABOUT_EXTENSION_NAME, AboutExtension, project
+  }
 }
