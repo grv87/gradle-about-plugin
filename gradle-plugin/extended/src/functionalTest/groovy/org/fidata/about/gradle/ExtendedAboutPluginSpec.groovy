5519f826ab52f8b8edf8b7a083a55d24ce244a22
Basil Peace <grv87@yandex.ru>
2019-06-10 23:09:46 +0300

    WIP

---
+++ gradle-plugin/extended/src/functionalTest/groovy/org/fidata/about/gradle/ExtendedAboutPluginSpec.groovy
@@ -0,0 +1,8 @@
+// SPDX-Copyright: ©  Basil Peace
+// SPDX-License-Identifier: Apache-2.0
+package org.fidata.about.gradle
+
+import spock.lang.Specification
+
+class ExtendedAboutPluginSpec extends Specification {
+}
